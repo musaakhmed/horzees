3ba755fc62bc201e8f4ea0c5e0e7ea887030c9d3
Musa Akhmedov <musa.punhan@pm.me>
2022-07-06 17:17:05 +0200

    commented Image tags in carousel component

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,68 +1,68 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 
-function Carousel() {
-    return (
-        <div className='h-screen flex items-center bg-bg-image bg-cover bg-center bg-purple-900 bg-blend-overlay'>
-            <div className='carousel carousel-center w-3/4 mx-auto p-4 space-x-4 bg-neutral rounded-box'>
-                <div className='carousel-item'>
-                    <Image
-                        height='250px'
-                        width='400px'
-                        src='/images/horzees/withBg/1.png'
-                        className='rounded-box'
-                    />
-                </div>
-                <div className='carousel-item'>
-                    <Image
-                        height='250px'
-                        width='400px'
-                        src='/images/horzees/withBg/2.png'
-                        className='rounded-box'
-                    />
-                </div>
-                <div className='carousel-item'>
-                    <Image
-                        height='250px'
-                        width='400px'
-                        src='/images/horzees/withBg/3.png'
-                        className='rounded-box'
-                    />
-                </div>
-                <div className='carousel-item'>
-                    <Image
-                        height='250px'
-                        width='400px'
-                        src='/images/horzees/withBg/4.png'
-                        className='rounded-box'
-                    />
-                </div>
-                <div className='carousel-item'>
-                    <Image
-                        height='250px'
-                        width='400px'
-                        src='/images/horzees/withBg/5.png'
-                        className='rounded-box'
-                    />
-                </div>
-                <div className='carousel-item'>
-                    <Image
-                        height='250px'
-                        width='400px'
-                        src='/images/horzees/withBg/6.png'
-                        className='rounded-box'
-                    />
-                </div>
-                <div className='carousel-item'>
-                    <Image
-                        height='250px'
-                        width='400px'
-                        src='/images/horzees/withBg/7.png'
-                        className='rounded-box'
-                    />
-                </div>
-            </div>
-        </div>
-    )
-}
+// function Carousel() {
+//     return (
+//         <div className='h-screen flex items-center bg-bg-image bg-cover bg-center bg-purple-900 bg-blend-overlay'>
+//             <div className='carousel carousel-center w-3/4 mx-auto p-4 space-x-4 bg-neutral rounded-box'>
+//                 <div className='carousel-item'>
+//                     <Image
+//                         height='250px'
+//                         width='400px'
+//                         src='/images/horzees/withBg/1.png'
+//                         className='rounded-box'
+//                     />
+//                 </div>
+//                 <div className='carousel-item'>
+//                     <Image
+//                         height='250px'
+//                         width='400px'
+//                         src='/images/horzees/withBg/2.png'
+//                         className='rounded-box'
+//                     />
+//                 </div>
+//                 <div className='carousel-item'>
+//                     <Image
+//                         height='250px'
+//                         width='400px'
+//                         src='/images/horzees/withBg/3.png'
+//                         className='rounded-box'
+//                     />
+//                 </div>
+//                 <div className='carousel-item'>
+//                     <Image
+//                         height='250px'
+//                         width='400px'
+//                         src='/images/horzees/withBg/4.png'
+//                         className='rounded-box'
+//                     />
+//                 </div>
+//                 <div className='carousel-item'>
+//                     <Image
+//                         height='250px'
+//                         width='400px'
+//                         src='/images/horzees/withBg/5.png'
+//                         className='rounded-box'
+//                     />
+//                 </div>
+//                 <div className='carousel-item'>
+//                     <Image
+//                         height='250px'
+//                         width='400px'
+//                         src='/images/horzees/withBg/6.png'
+//                         className='rounded-box'
+//                     />
+//                 </div>
+//                 <div className='carousel-item'>
+//                     <Image
+//                         height='250px'
+//                         width='400px'
+//                         src='/images/horzees/withBg/7.png'
+//                         className='rounded-box'
+//                     />
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
-export default Carousel
+// export default Carousel
--- pages/art.js
+++ pages/art.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
